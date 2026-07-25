02277f520f39929d9c1ee518820e367af43b42f1
BAKHRULLO <nbcapital.uz@gmail.com>
2026-07-25 09:37:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Unutilmas Ta'm — Service Worker */
-const CACHE = 'unutilmas-v36';
+const CACHE = 'unutilmas-v37';
 const APP_SHELL = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
